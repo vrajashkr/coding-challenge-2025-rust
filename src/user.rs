f6ba4267fa802c61f4e808978dad32ae78b077f3
Vishwas Rajashekar <dev@vrajashkr.com>
2025-10-19 23:45:15 +0530

    initial version with u64

--- src/user.rs
+++ src/user.rs
@@ -1,14 +1,114 @@
+use std::collections::HashMap;
+
 use crate::parameter::Parameter;
 
-/// Please only modify this file. You might add another files, but don't
-///     modify the other existing files :). All existing files will be replaced
-///     by the default implementation during evaluation.
-#[allow(unused_variables)]
-pub fn build_idx(parameter: &Parameter, data: &[i32]) -> Box<[u8]> {
-    unimplemented!("build_idx is not implemented");
+// index representation is:
+// i32 - min
+// i32 - max
+// u64 - number of entries
+// multiple instances of:
+// i32 - value
+// u64 - count
+
+struct BlockIndex {
+    min: i32,
+    max: i32,
+    counts: HashMap<i32, u64>,
 }
 
-#[allow(unused_variables)]
-pub fn query_idx(parameter: &Parameter, index: &[u8], query: &i32) -> Option<u64> {
-    unimplemented!("query_idx is not implemented");
+impl Into<Box<[u8]>> for BlockIndex {
+    fn into(self) -> Box<[u8]> {
+        let mut store: Vec<u8> = Vec::new();
+
+        store.append(&mut self.min.to_le_bytes().to_vec());
+        store.append(&mut self.max.to_le_bytes().to_vec());
+
+        let mut num_entries = self.counts.len().to_le_bytes().to_vec();
+        store.append(&mut num_entries);
+
+        for (key, val) in &self.counts {
+            store.append(&mut key.to_le_bytes().to_vec());
+            store.append(&mut val.to_le_bytes().to_vec());
+        }
+
+        store.into_boxed_slice()
+    }
+}
+
+impl From<&[u8]> for BlockIndex {
+    fn from(value: &[u8]) -> Self {
+        let min = i32::from_le_bytes(value[0..4].try_into().unwrap());
+        let max = i32::from_le_bytes(value[4..8].try_into().unwrap());
+        let num_entries = u64::from_le_bytes(value[8..16].try_into().unwrap());
+
+        let mut map: HashMap<i32, u64> = HashMap::new();
+
+        // starts reading at byte 16 offset
+        // each entry is 12 bytes
+        for i in 0..num_entries as usize {
+            let entry_start = 16 + i * 8;
+            let key_end = entry_start + 4;
+            let value_start = key_end.clone();
+            let entry_end = value_start + 8;
+            let key = i32::from_le_bytes(value[entry_start..key_end].try_into().unwrap());
+            let value = u64::from_le_bytes(value[value_start..entry_end].try_into().unwrap());
+
+            map.insert(key, value);
+        }
+
+        BlockIndex {
+            min: min,
+            max: max,
+            counts: map,
+        }
+    }
+}
+
+pub fn build_idx(_parameter: &Parameter, data: &[i32]) -> Box<[u8]> {
+    // the index will have the min and max in the block along with counts for each of the elements
+    // in the block.
+
+    let mut min = i32::MAX;
+    let mut max = i32::MIN;
+
+    let mut map: HashMap<i32, u64> = HashMap::new();
+
+    data.into_iter().for_each(|num| {
+        if *num > max {
+            max = *num;
+        }
+
+        if *num < min {
+            min = *num;
+        }
+
+        match map.get(num) {
+            Some(count) => map.insert(*num, count + 1),
+            None => map.insert(*num, 1),
+        };
+    });
+
+    BlockIndex {
+        min: min,
+        max: max,
+        counts: map,
+    }
+    .into()
+}
+
+pub fn query_idx(_parameter: &Parameter, index: &[u8], query: &i32) -> Option<u64> {
+    // check the index to see if the value is in range, otherwise, return None
+
+    let block_idx: BlockIndex = index.into();
+
+    if *query > block_idx.min && *query < block_idx.max {
+        let result = match block_idx.counts.get(query) {
+            Some(count) => Some(*count),
+            None => None,
+        };
+
+        return result;
+    }
+
+    Some(0)
 }
